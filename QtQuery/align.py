6c6da3c4ad99b15c391ee6e802526da83141a05a
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-05-19 19:12:16 +0000

    AlignedMeta

--- QtQuery/align.py
+++ QtQuery/align.py
@@ -17,7 +17,11 @@
 # You should have received a copy of the GNU General Public License
 # along with QtQuery. If not, see <http://www.gnu.org/licenses/>.
 
+from six import with_metaclass
+
 __all__ = ['ALIGNMENT', 'Alignment', 'Aligned']
+
+from moretools import cached
 
 from . import Q, QtCore
 
@@ -43,7 +47,37 @@ class Alignment(QtCore.Qt.Alignment):
         QtCore.Qt.Alignment.__init__(self, qalign)
 
 
-class Aligned(object):
+class AlignedMeta(type):
+    @cached
+    def __getitem__(cls, _qalign):
+        class Aligned(cls):
+            qalign = Alignment(_qalign)
+
+            def __init__(self, q):
+                self.q = Q(q)
+
+        Aligned.__module__ = cls.__module__
+        Aligned.__name__ = '%s[%s]' % (cls.__name__, repr(_qalign))
+        return Aligned
+
+    def __getattr__(cls, name):
+        class Proxy(object):
+            def __init__(self, consumer, attr):
+                self.consumer = consumer
+                ## self.attr = getattr(Q, attrname)
+                self.attr = attr
+
+            def __getitem__(self, arg):
+                return type(self)(self.consumer, self.attr[arg])
+
+            def __call__(self, *args, **kwargs):
+                q = self.attr(*args, **kwargs)
+                return self.consumer(q)
+
+        return Proxy(cls, getattr(Q, name))
+
+
+class Aligned(with_metaclass(AlignedMeta, object)):
     def __init__(self, qalign, q):
         self.qalign = Alignment(qalign)
         self.q = Q(q)
