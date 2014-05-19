564e759ba41d5702aa54a67a5cf98630132a51a9
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-05-19 19:43:44 +0000

    Q.id

--- QtQuery/__init__.py
+++ QtQuery/__init__.py
@@ -42,7 +42,47 @@ QType = type(QtCore.QObject)
 QSignal = QtCore.pyqtBoundSignal
 
 
+class ID(object):
+    def __init__(self, Q):
+        self.Q = Q
+
+    def __getitem__(self, id):
+        self.id = id
+        return self
+
+    def __call__(self, q):
+        q = self.Q(q)
+        q.id = self.id
+        return q
+
+    def __getattr__(self, name):
+        class Proxy(object):
+            def __init__(self, consumer, attr):
+                self.consumer = consumer
+                self.attr = attr
+
+            def __getitem__(self, arg):
+                return type(self)(self.consumer, self.attr[arg])
+
+            def __getattr__(self, name):
+                return type(self)(self.consumer, getattr(self.attr, name))
+
+            def __call__(self, *args, **kwargs):
+                q = self.attr(*args, **kwargs)
+                return self.consumer(q)
+
+        def consumer(q):
+            q.id = self.id
+            return q
+
+        return Proxy(consumer, getattr(Q, name))
+
+
 class QMeta(type):
+
+    @property
+    def id(Q):
+        return ID(Q)
 
     def label(Q, qlabel):
         if isinstance(qlabel, Q.Label.qclass):
