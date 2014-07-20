71bf23192c44d4d71f687427c6fccbec3e9549f0
Stefan Zimmermann <zimmermann.code@gmail.com>
2014-07-20 15:07:26 +0000

    zetup

--- setup.py
+++ setup.py
@@ -1,27 +1,8 @@
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+# Run the zetup script (reading config from zetuprc):
+exec(open('__init__.py').read())
 
 
-VERSION = open('VERSION').read().strip()
-
-REQUIRES = open('requirements.txt').read()
-
-
-setup(
-  name='QtQuery',
-  version=VERSION,
-  description="A jQuery inspired Pythonic Qt experience.",
-
-  author='Stefan Zimmermann',
-  author_email='zimmermann.code@gmail.com',
-  url='http://bitbucket.org/userzimmermann/QtQuery',
-
-  license='GPLv3',
-
-  install_requires=REQUIRES,
-
+zetup(
   packages=[
     'QtQuery',
     ],
